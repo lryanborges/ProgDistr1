70f131810caf3557e03a8af24684510b75be0292
lryanborges <ryanborgeslockser@gmail.com>
2024-08-22 11:55:54 -0300

    alt: 4 threads pra inicialização de cada uma das conexões do storage

---
+++ src/threads/ConnectionThread.java
@@ -0,0 +1,19 @@
+package threads;
+
+import java.util.concurrent.Callable;
+
+import functional_interfaces.Connection;
+
+public class ConnectionThread implements Callable<String>{
+	
+	Connection conn = () -> {
+		
+	};
+	
+	@Override
+	public String call() throws Exception {
+		
+		return null;
+	}
+
+}
